9f89d8d3fd7f7c46b40d492ea299911bc36f9e33
Brad Kittinger <123213712+kittbg@users.noreply.github.com>
2023-03-13 15:19:00 -1000

    Red and Black Checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Your JS goes here
+
+// Create container element
+const container = document.createElement('div');
+container.style.width = '800px';
+container.style.margin = 'auto'
+
+for (var i = 0; i < 81; i++){
+let tile = document.createElement('div');
+tile.classList.add('tile')
+tile.style.width = '11.1%';
+tile.style.paddingBottom = '11.1%';
+tile.style.float = 'left'
+
+if ((Math.floor(i / 81) + i) % 2 === 0)  {
+  tile.style.backgroundColor = 'black';
+  } else {
+    tile.style.backgroundColor = 'red';
+ }
+
+ // Append tile to container
+ container.appendChild(tile);
+}
+ 
+// Append container to parent element
+document.body.appendChild(container);
+
